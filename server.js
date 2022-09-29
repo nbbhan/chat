07e579e9765ec865326a4389cf8c84f89633f146
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-09-29 11:34:29 +0900

    new

---
+++ server.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql `
+    type Query {
+        "Simple start!"
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'world'
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
