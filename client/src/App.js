1b6af52c2b9ba1b1ad35854709c7deff18804c78
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-10-06 14:25:50 +0900

    new

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+
+import './App.css';
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Login.js
@@ -0,0 +1,7 @@
+function Login() {
+    return ( 
+        <h1>Quan loves Barori</h1>
+    );
+}
+
+export default Login;
