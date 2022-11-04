e6b639e3e4f7516b1278d3a661287456a5897126
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-11-04 10:40:37 +0900

    new

--- client/src/Components/Slide/index.js
+++ client/src/Components/Slide/index.js
@@ -1,7 +1,47 @@
 function Slide() {
     return (
         <div className="slide">
-            
+            <div className="slide-title">
+                人気商品
+            </div>
+
+            <div className="slide-container">
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+
+                <div className="slide-container-item">
+
+                </div>
+            </div>
         </div>
     );
 }
