252a994847e6d739ee469a3bbd1fb3cff99a2f86
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-11-09 09:32:38 +0900

    new

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const typeDefs = require('./graphql/typeDefs')
 const contextMiddleware = require('./util/contextMiddleware')
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: contextMiddleware,
+    typeDefs,
+    resolvers,
+    context: contextMiddleware,
 })
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
+    console.log(`🚀 Server ready at ${url}`)
 
-  sequelize
-    .authenticate()
-    .then(() => console.log('Database connected!!'))
-    .catch((err) => console.log(err))
+    sequelize
+        .authenticate()
+        .then(() => console.log('Database connected!!'))
+        .catch((err) => console.log(err))
 })
