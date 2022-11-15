592b8778f3cf17276779747ecd9380d434170322
nbbhan <114631346+nbbhan@users.noreply.github.com>
2022-11-15 15:15:39 +0900

    new

--- client/src/Components/Prefectures/index.js
+++ client/src/Components/Prefectures/index.js
@@ -1,7 +1,22 @@
 function Prefectures() {
     return (
         <div className="prefectures">
-            <div></div>
+            <div>
+                <select>
+                    <optgroup label="Group 1">
+                        <option>Option 1.1</option>
+                    </optgroup>
+                    <optgroup label="Group 2">
+                        <option>Option 2.1</option>
+                        <option>Option 2.2</option>
+                    </optgroup>
+                    <optgroup label="Group 3" disabled>
+                        <option>Option 3.1</option>
+                        <option>Option 3.2</option>
+                        <option>Option 3.3</option>
+                    </optgroup>
+                </select>
+            </div>
         </div>
     )
 }
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -87,6 +87,29 @@ function Home() {
                         <Slide arr={data} name="人気商品" icon={flame} />
                     </div>
 
+                    <div className="home-container-item-left-contents-ogino">
+                        <div className="home-container-item-middle-between-ogino">
+                            <div>海沿いの町、山間の村々</div>
+                            <div>土地に根付く伝統、代々受け継いできた文化</div>
+                            <div>それらが織りなす一品</div>
+                        </div>
+
+                        <div className="home-container-item-middle-between-ogino">
+                            <div>ご存知の有名な品も</div>
+                            <div>まだ注目を浴びてない品も</div>
+                            <div>その一つ一つがその土地土地を連想させてくれる</div>
+                        </div>
+
+                        <div className="home-container-item-middle-between-ogino">
+                            <div>そういった特産品を</div>
+                            <div>皆様のもとへお届けいたします</div>
+                        </div>
+                        
+                        <div className="home-container-item-middle-between-ogino">
+                            どうぞ下記よりご照覧ください
+                        </div>
+                    </div>
+
                     <div className="home-container-item">
                         <Prefectures />
                     </div>
