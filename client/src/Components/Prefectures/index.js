3fa084a2a0231b84e68363c7beb70c71766289ab
nbbhan <114631346+nbbhan@users.noreply.github.com>
2022-11-17 15:15:09 +0900

    new

--- client/src/Components/Prefectures/index.js
+++ client/src/Components/Prefectures/index.js
@@ -2,15 +2,15 @@ import { useContext } from 'react'
 
 import { Data } from '../../Provider'
 
+import specialties from '../../Imgs/back1.png'
+
 function Prefectures() {
     const myData = useContext(Data)
 
     return (
         <div className="prefectures">
             <div className="prefectures-items">
-                <div className="prefectures-title">
-                    森のやさしさと医療の⼿をつないで、 どうぶつが「どうぶつ」らしくある場所へ。
-                </div>
+                <div className="prefectures-title">全国各地の気候風土を生かした数々の名産品を皆様へお届けします。</div>
                 <div className="prefectures-item">
                     <div>海沿いの町、山間の村々</div>
                     <div>土地に根付く伝統、代々受け継いできた文化</div>
@@ -27,7 +27,23 @@ function Prefectures() {
                 </div>
                 <div className="prefectures-item">どうぞ下記よりご照覧ください</div>
             </div>
-            <div className="prefectures-container">123</div>
+
+            <div className="prefectures-container">
+                <div className="prefectures-container-img">
+                    <img alt="写真ですよ" src={specialties} />
+                </div>
+
+                <div className="prefectures-container-items">
+                    <div className="prefecturs-container-item">北海道・東北</div>
+                    <div className="prefecturs-container-item">関東</div>
+                    <div className="prefecturs-container-item">中部</div>
+                    <div className="prefecturs-container-item">近畿</div>
+
+                    <div className="prefecturs-container-item">中国</div>
+                    <div className="prefecturs-container-item">四国</div>
+                    <div className="prefecturs-container-item">九州</div>
+                </div>
+            </div>
         </div>
     )
 }
