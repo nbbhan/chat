129d1a661c1da72024060d6274c56735aa39712c
jake0102 <94685129+jake0102@users.noreply.github.com>
2022-12-09 09:04:07 +0900

    new

--- client/src/Components/Prefectures/index.js
+++ client/src/Components/Prefectures/index.js
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState } from 'react'
 
-import { Data } from '../../Provider'
+import { Data } from '~/Provider'
 
-import prefectures1 from '../../Imgs/prefectures-1.png'
+import prefectures1 from '~/Imgs/prefectures-1.png'
+import prefectures2 from '~/Imgs/prefectures-2.png'
 
 import Items from './Items'
 import Main from './Main'
@@ -10,8 +11,28 @@ import Main from './Main'
 function Prefectures() {
     const myData = useContext(Data)
 
+    let x
+
+    const imgArr = [
+        // 北海道
+        prefectures1,
+        // 四国
+        prefectures2,
+    ]
+
     const handleSelectValue = (e) => {
         myData.handleSelect(e.target.value)
+    }
+
+    switch (myData.select) {
+        case '北海道・東北':
+            x = 0
+            break
+        case '四国':
+            x = 1
+            break
+        default:
+            break
     }
 
     useEffect(() => {}, [myData.select])
@@ -22,9 +43,7 @@ function Prefectures() {
 
             <div className="prefectures-container">
                 <div className="prefectures-container-left">
-                    {/* {myData.select} */}
-
-                    <img src={prefectures1} />
+                    <img src={imgArr[x]} />
                 </div>
 
                 <div className="prefectures-container-right">
