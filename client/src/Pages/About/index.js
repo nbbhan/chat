1722eb6b225c195783eb566e35117f8fdab23fdb
nbbhan <114631346+nbbhan@users.noreply.github.com>
2023-01-17 14:40:42 +0900

    new

--- client/src/Pages/About/index.js
+++ client/src/Pages/About/index.js
@@ -24,7 +24,25 @@ function About() {
                     <div className="about-main">
                         <Title props={inf} />
 
-                        <div className="about-main-container"></div>
+                        <div className="about-main-container">
+                            QOKSの由来について
+                            <div></div>
+                            ～制作チーム全員の頭文字をとってQOKSは生まれました。
+                            <div></div>
+                            QOKSの掲げる目標と効果
+                            <div></div>
+                            ～経済の停滞した現状の打開のため国消国産の精神のもと農畜産業のみならずそのほかの産業においてもMADE　IN　JAPANの製品を取り扱っています。
+                            <div></div>
+                            　それにより販売者は販売経路の拡大とそれに伴う売上の増加を見込め、利用者は遠方の故郷や、訪れたことのない土地の特産品を得ることができる。
+                            <div></div>
+                            　これを目標としてQOKSを開発しました。
+                            <div></div>
+                            QOKSの機能について
+                            <div></div>
+                            ～チャット機能を使用することでユーザーとコミュニケーションをとる事ができ、その他にポイント、アカウント機能などがあります。
+                            <div></div>
+                            　また画面遷移や商品情報取得時間が高速化されており、スムーズにお買い物をする事が可能です。
+                        </div>
                     </div>
                 </div>
             </div>
