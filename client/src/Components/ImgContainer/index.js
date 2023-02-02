bf8c474b21d98f8b87769f5a0d77a5af7c759181
nbbhan <114631346+nbbhan@users.noreply.github.com>
2023-02-02 13:23:40 +0900

    new

--- client/src/Components/ImgContainer/index.js
+++ client/src/Components/ImgContainer/index.js
@@ -1,13 +1,15 @@
 import long from '~/Imgs/longback.png'
+import long_dark from '~/Imgs/longback_dark.png'
 
 function ImgContainer() {
+    const storage = window.localStorage.getItem('theme')
     return (
         <div className="img-container">
-            <img src={long} />
+            <img src={storage === 'dark' ? long_dark : long} />
 
             <div className="img-text">
-                <div className="img-text-main" style={{ transform: 'translateY(15%)' }}>
-                    しゃべって、楽しむ。
+                <div className="img-text-main" style={{ transform: 'translateY(22.5%)' }}>
+                    話して、楽しむ。
                 </div>
 
                 <div className="img-text-main">売って、買って、</div>
