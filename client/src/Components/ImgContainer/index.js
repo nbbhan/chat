7774b205193d7ef9656f494f1885f6f615d61553
jake0102 <94685129+jake0102@users.noreply.github.com>
2023-02-02 13:08:39 +0900

    new

--- client/src/Components/ImgContainer/index.js
+++ client/src/Components/ImgContainer/index.js
@@ -1,13 +1,17 @@
-import first from '~/Imgs/1.png'
-import second from '~/Imgs/2.png'
-import third from '~/Imgs/3.png'
-import four from '~/Imgs/4.png'
 import long from '~/Imgs/longback.png'
 
 function ImgContainer() {
     return (
         <div className="img-container">
             <img src={long} />
+
+            <div className="img-text">
+                <div className="img-text-main" style={{ transform: 'translateY(15%)' }}>
+                    しゃべって、楽しむ。
+                </div>
+
+                <div className="img-text-main">売って、買って、</div>
+            </div>
         </div>
     )
 }
